a22617baafc93b6774b9bbf66010ccb81b71e90d
ParkSoobin1219 <psb1219j@gmail.com>
2022-10-11 16:53:21 +0900

    06_03

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func1(void){
-   int x;
-   printf("func 1 x is at %p\n", &x);
+int sumTwo(int a,int b){
+   int sum;
+   sum=a+b;
+   return sum;
 }
+
+int square(int n){
+   int square;
+   square=n*n;
+   return square;
+}
+
+int get_max(int x,int y){
+   if(x<y)
+     return y;
+   else if(x>y)
+     return x;
+}
+
 
 int main(int argc, char *argv[]) 
 {
-   int x;
-   printf("main x is at %p\n", &x);
-   func1();
+   int x,y;
+   
+   printf("input 2 integers: ");
+   scanf("%d%d", &x,&y);
+   
+   printf("the sum of 2 integers is %d\n", sumTwo(x,y));
+   printf("the square of the 1st integer is %d\n", square(x));
+   printf("the square of the 2nd integer is %d\n", square(y));
+   printf("the bigger integer is %d\n", get_max(x,y));
    
    return 0;
 }
