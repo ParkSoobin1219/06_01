becf72cb5472276c4b177896a98c038b0126c9ff
ParkSoobin1219 <psb1219j@gmail.com>
2022-10-11 17:03:45 +0900

    06_04

--- main.c
+++ main.c
@@ -1,37 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo(int a,int b){
-   int sum;
-   sum=a+b;
-   return sum;
+int square(int a){
+  return (a*a);
 }
-
-int square(int n){
-   int square;
-   square=n*n;
-   return square;
-}
-
-int get_max(int x,int y){
-   if(x<y)
-     return y;
-   else if(x>y)
-     return x;
-}
-
 
 int main(int argc, char *argv[]) 
 {
-   int x,y;
-   
-   printf("input 2 integers: ");
-   scanf("%d%d", &x,&y);
-   
-   printf("the sum of 2 integers is %d\n", sumTwo(x,y));
-   printf("the square of the 1st integer is %d\n", square(x));
-   printf("the square of the 2nd integer is %d\n", square(y));
-   printf("the bigger integer is %d\n", get_max(x,y));
-   
+   int a=2;
+   a=square(a);
+   printf("a=%d\n", a);
    return 0;
 }
