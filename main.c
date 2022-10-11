94eea7db6abd10ba565e0e9e54d581e3cea6e6fd
ParkSoobin1219 <psb1219j@gmail.com>
2022-10-11 17:05:17 +0900

    06_05

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a){
-  return (a*a);
-}
+int get_integer();
+int factorial(int a);
+int combination(int x, int y);
 
 int main(int argc, char *argv[]) 
 {
-   int a=2;
-   a=square(a);
-   printf("a=%d\n", a);
+   int n,r;
+   
+   n=get_integer();
+   r=get_integer();
+   
+   printf("the reseult is %d", combination(n,r));
+
    return 0;
+}
+
+int get_integer(){
+   int n;
+   printf("input an integer:");
+   scanf("%d", &n);
+   return n;
+}
+
+int factorial(int a){
+   int res=1;
+   int i=0;
+   
+   for(i=1;i<(a+1);i++)
+     res=res*i;
+     
+   return res;
+}
+
+int combination(int x, int y){
+   int combi;
+   combi=factorial(x)/factorial(x-y)/factorial(y);
+   return combi;
 }
